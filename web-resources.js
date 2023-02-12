dd88c152881f92dd1735ef323c125b1e1680c829
stef totino <stefantotino@gmail.com>
2023-02-11 23:58:07 -0500

    create bare bones pptx in the root dir

--- web-resources.js
+++ web-resources.js
@@ -1,10 +1,22 @@
-const [serverless, express] = [require('serverless-http'), require('express')];
-const [app, bodyParser] = [express(), require('body-parser')];
+const serverless = require('serverless-http');
+const express = require('express');
+const pptxgen = require('pptxgenjs');
+const app = express();
+const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/index.html', async (req, res) => {
-    await res.send("<h1>ZECTR</h1><div>2023<div/>")
+    let pres = new pptxgen();
+    let slide = pres.addSlide();
+    let textboxText = "Hello World from PptxGenJS!";
+    let textboxOpts = { x: 1, y: 1, color: "363636" };
+
+    slide.addText(textboxText, textboxOpts);
+    pres.writeFile();
+
+    res.send("<h1>ZECTR</h1><div>2023<div/>")
 })
 
 module.exports.handler = serverless(app);
