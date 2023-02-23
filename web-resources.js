7398cd5e78b9426c2393163d45d68253a723f000
stef totino <stefantotino@gmail.com>
2023-02-22 21:33:41 -0500

    list s3 buckets

--- web-resources.js
+++ web-resources.js
@@ -3,6 +3,7 @@ const express = require('express');
 const pptxgen = require('pptxgenjs');
 const app = express();
 const bodyParser = require('body-parser');
+const AWS = require('aws-sdk');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,7 +17,24 @@ app.get('/index.html', async (req, res) => {
     slide.addText(textboxText, textboxOpts);
     pres.writeFile();
 
-    res.send("<h1>ZECTR</h1><div>2023<div/>")
+    // AWS.config.update({ region: 'us-east-1' });
+    // aws configure list (CLI)
+    // TODO: this configuration needs to be parsed at runtime using a proper external file or a database.
+
+    var s3 = new AWS.S3();
+    var listBucketsResult = {};
+    s3.listBuckets(function (err, data) {
+        if (err) {
+            listBucketsResult = JSON.stringify(err);
+        } else {
+            listBucketsResult = data.Buckets.map(function (bucket) {
+                return bucket.Name;
+            });
+        }
+
+        var html = `<h1>ZECTR</h1><h3>export</h3><div>${listBucketsResult}</div>`;
+        res.send(html)
+    });
 })
 
 module.exports.handler = serverless(app);
