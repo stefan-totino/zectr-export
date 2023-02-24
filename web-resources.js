3f2423469367ac58e50e7fb68f5614af90c6e82b
stef totino <stefantotino@gmail.com>
2023-02-24 17:23:27 -0500

    new syntax for endpoint(s) in yml file

--- web-resources.js
+++ web-resources.js
@@ -5,17 +5,27 @@ const app = express();
 const bodyParser = require('body-parser');
 const AWS = require('aws-sdk');
 
+/* 
+TODO:
+configuration file for AWS parameters
+write ppts and xls to bucket
+accept multiple commands over HTTP
+database?
+begin to encode dad's language and business logic
+error handling (result object)
+RUST integration
+*/
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/index.html', async (req, res) => {
-    let pres = new pptxgen();
-    let slide = pres.addSlide();
-    let textboxText = "Hello World from PptxGenJS!";
-    let textboxOpts = { x: 1, y: 1, color: "363636" };
+    var html = `<h1>ZECTR</h1><h3>export</h3><div>2023</div>`;
+    res.send(html)
+})
 
-    slide.addText(textboxText, textboxOpts);
-    pres.writeFile();
+app.get('/buckets', async (req, res) => {
 
     // AWS.config.update({ region: 'us-east-1' });
     // aws configure list (CLI)
@@ -32,9 +42,21 @@ app.get('/index.html', async (req, res) => {
             });
         }
 
-        var html = `<h1>ZECTR</h1><h3>export</h3><div>${listBucketsResult}</div>`;
+        var html = `<div>${listBucketsResult}</div>`;
         res.send(html)
     });
+})
+
+app.get('/slides', async (req, res) => {
+    let pres = new pptxgen();
+    let slide = pres.addSlide();
+    let textboxText = "Hello World from PptxGenJS!";
+    let textboxOpts = { x: 1, y: 1, color: "363636" };
+
+    slide.addText(textboxText, textboxOpts);
+    pres.writeFile();
+
+    res.send()
 })
 
 module.exports.handler = serverless(app);
