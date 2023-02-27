65591de5a5ed0807c4f13139c2bff4a6b0fe97bb
stef totino <stefantotino@gmail.com>
2023-02-26 21:37:45 -0500

    s3 upload init

--- web-resources.js
+++ web-resources.js
@@ -1,38 +1,59 @@
-const serverless = require('serverless-http');
-const express = require('express');
-const pptxgen = require('pptxgenjs');
-const app = express();
-const bodyParser = require('body-parser');
-const AWS = require('aws-sdk');
+'use strict'
+
+/*
+    https://expressjs.com/
+    https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/s3-example-creating-buckets.html#s3-example-creating-buckets-upload-file
+*/
 
 /* 
 TODO:
-configuration file for AWS parameters
-write ppts and xls to bucket
-accept multiple commands over HTTP
-database?
-begin to encode dad's language and business logic
-error handling (result object)
-RUST integration
+    configuration file for AWS parameters. write ppts and xls to bucket
+    accept multiple commands over HTTP. database?
+    begin to encode dad's language and business logic. error handling (result object)
+    RUST integration. JWT Auth
 */
 
+const serverless = require('serverless-http');
+const express = require('express');
+const pptxgen = require('pptxgenjs');
+const bodyParser = require('body-parser');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+
+// TODO: where should i scope these?
+const s3 = new AWS.S3();
+const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+function uploadS3(bucketName, file) {
+    // TODO: console notifications need to be rolled into some kind of result object
+    let uploadParams = { Bucket: bucketName, Key: '', Body: '' };
+    let fileStream = fs.createReadStream(file);
+
+    fileStream.on('error', function (err) {
+        console.log('File Error', err);
+    });
+    uploadParams.Body = fileStream;
+    uploadParams.Key = path.basename(file);
+    s3.upload(uploadParams, function (err, data) {
+        if (err) {
+            console.log("Error", err);
+        } if (data) {
+            console.log("Upload Success", data.Location);
+        }
+    });
+}
+
 app.get('/index.html', async (req, res) => {
-    var html = `<h1>ZECTR</h1><h3>export</h3><div>2023</div>`;
+    let html = `<h1>ZECTR</h1><h3>export</h3><div>2023</div>`;
     res.send(html)
 })
 
 app.get('/buckets', async (req, res) => {
-
-    // AWS.config.update({ region: 'us-east-1' });
-    // aws configure list (CLI)
-    // TODO: this configuration needs to be parsed at runtime using a proper external file or a database.
-
-    var s3 = new AWS.S3();
-    var listBucketsResult = {};
+    let listBucketsResult = {};
     s3.listBuckets(function (err, data) {
         if (err) {
             listBucketsResult = JSON.stringify(err);
@@ -41,22 +62,20 @@ app.get('/buckets', async (req, res) => {
                 return bucket.Name;
             });
         }
-
         var html = `<div>${listBucketsResult}</div>`;
         res.send(html)
     });
 })
 
-app.get('/slides', async (req, res) => {
+app.post('/slides', (req, res) => {
     let pres = new pptxgen();
     let slide = pres.addSlide();
     let textboxText = "Hello World from PptxGenJS!";
     let textboxOpts = { x: 1, y: 1, color: "363636" };
-
     slide.addText(textboxText, textboxOpts);
     pres.writeFile();
-
-    res.send()
+    uploadS3("zectr-io-build", "Presentation.pptx");
+    res.json({ requestBody: req.body })
 })
 
 module.exports.handler = serverless(app);
